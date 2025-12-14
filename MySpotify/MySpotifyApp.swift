fac05930b909e165f4627975fc5b014021adcf20
Harixlr8 <harikrishnan5432@gmail.com>
2025-12-14 19:11:18 +0530

    Initial Commit

---
+++ MySpotify/MySpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySpotifyApp.swift
+//  MySpotify
+//
+//  Created by Harikrishnan on 14/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MySpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
